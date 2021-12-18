4d0f743ca826726f5b4d301d2bb71ce91bb4c049
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-12-18 17:10:12 -0300

    Changes

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -7,6 +7,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
+        name: 'teste',
         message: 'Go Serverless v1.0! Your function executed successfully!',
         resultCalculeted: calculator.sum(10, 10),
         urlApi: process.env.URL_API
